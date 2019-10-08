861a2b2052b77991636721ec90afd253163eca42
Paulo Soares <pauloxti@gmail.com>
2019-10-08 17:44:16 +0200

    funcionando con mongo

--- server/models/user.js
+++ server/models/user.js
@@ -1,24 +1,37 @@
 let mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 
-var User = new Schema({
+var User = new Schema(
+  {
+    invited_by: {
+      type: String,
+      required: [true, 'invited_by is required'],
+      lowercase: true
+    },
     name: {
-        type: String,
-        required : [ true, 'name is required'],
-        lowercase : true
+      type: String,
+      required: [true, 'name is required'],
+      lowercase: true
     },
     email: {
-        type: String,
-        required : [ true, 'email is required'],
-        unique : true,
-        lowercase : true
+      type: String,
+      required: [true, 'email is required'],
+      unique: true,
+      lowercase: true
     },
     password: {
-        type: String,
-        required : [ true, 'password is required']
+      type: String,
+      required: [true, 'password is required']
+    },
+    invited_list: {
+      type: Array,
+      required: false,
+      lowercase: true
     }
-}, {
+  },
+  {
     timestamps: true
-});
+  }
+);
 
 module.exports = mongoose.model('User', User);
--- server/services/v1/auth/auth.js
+++ server/services/v1/auth/auth.js
@@ -1,108 +1,116 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const {validationResult} = require('express-validator/check');
+const { validationResult } = require('express-validator/check');
 
 const config = require('../../../config/env_config/config');
 const userModel = require('../../../models/user');
 
 const register = async (req, res, next) => {
-    const errors = validationResult(req);
+  const errors = validationResult(req);
 
-    if (!errors.isEmpty()) {
-        return res.status(422).json({errors: errors.array()});
-    }
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
 
-    let {name, email, password} = req.body;
+  let { name, email, password } = req.body;
 
-    let isEmailExists = await userModel.findOne({"email": email});
+  let isEmailExists = await userModel.findOne({ email: email });
 
-    if (isEmailExists) {
-        return res.status(409).json({
-            "errors": [{
-                "msg": "email already exists"
-            }]
-        })
-    }
-
-    let hashedPassword = await bcrypt.hash(password, 8);
-
-    try {
-        let user = await userModel.create({
-            name: name,
-            email: email,
-            password: hashedPassword
-        });
-
-        if (!user) {
-            throw new error();
+  if (isEmailExists) {
+    return res.status(409).json({
+      errors: [
+        {
+          msg: 'email already exists'
         }
+      ]
+    });
+  }
 
-        return res.status(201).json({
-            "success": [{
-                "msg": "user registered successfully"
-            }]
-        });
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json(
-            {
-                "errors": [{
-                    "msg": "there was a problem registering a user."
-                }]
-            }
-        );
+  let hashedPassword = await bcrypt.hash(password, 8);
+
+  try {
+    let user = await userModel.create({
+      invited_by: invited_by,
+      name: name,
+      email: email,
+      password: hashedPassword,
+      invited_list: []
+    });
+
+    if (!user) {
+      throw new error();
     }
 
-
+    return res.status(201).json({
+      success: [
+        {
+          msg: 'user registered successfully'
+        }
+      ]
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      errors: [
+        {
+          msg: 'there was a problem registering a user.'
+        }
+      ]
+    });
+  }
 };
 
 const login = async (req, res, next) => {
+  const errors = validationResult(req);
 
-    const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
 
-    if (!errors.isEmpty()) {
-        return res.status(422).json({errors: errors.array()});
+  let { email, password } = req.body;
+
+  try {
+    let isUserExists = await userModel.findOne({ email: email });
+
+    let isPasswordValid = await bcrypt.compare(password, isUserExists.password);
+
+    if (!isUserExists || !isPasswordValid) {
+      return res.status(401).json({
+        errors: [
+          {
+            msg: 'email/password is wrong'
+          }
+        ]
+      });
     }
 
-    let {email, password} = req.body;
+    let token = jwt.sign({ id: isUserExists._id }, config.secret, {
+      expiresIn: 86400
+    });
 
-    try {
-        let isUserExists = await userModel.findOne({"email": email});
-
-        let isPasswordValid = await bcrypt.compare(password, isUserExists.password);
-
-        if (!isUserExists || !isPasswordValid) {
-            return res.status(401).json({
-                "errors": [{
-                    "msg": "email/password is wrong"
-                }]
-            })
+    res.status(200).json({
+      success: [
+        {
+          msg: 'user login successfully',
+          email: email,
+          token: token
         }
-
-        let token = jwt.sign({id: isUserExists._id}, config.secret, {expiresIn: 86400});
-
-        res.status(200).json({
-            "success": [{
-                "msg": "user login successfully",
-                "email": email,
-                "token": token
-            }]
-        })
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json(
-            {
-                "errors": [{
-                    "msg": "there was a problem login a user."
-                }]
-            }
-        );
-    }
-
+      ]
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      errors: [
+        {
+          msg: 'there was a problem login a user.'
+        }
+      ]
+    });
+  }
 };
 
 module.exports = {
-    register: register,
-    login: login
+  register: register,
+  login: login
 };
